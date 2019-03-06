9dcd6a517d95ed5813e9d783f6370bb915d3266d
Nick Pizzo <nick.pizzo@gmail.com>
2019-03-06 15:32:17 -0800

    global styles and navbar

---
+++ src/components/Globals/Navbar.js
@@ -0,0 +1,41 @@
+import { Link } from "gatsby"
+import React, { useState } from "react"
+import { FaCartArrowDown } from "react-icons/fa"
+import logo from "../../images/logo.svg"
+
+export default function Navbar() {
+  const [toggleNav, setToggleNav] = useState(false)
+
+  return (
+    <nav className="navbar navbar-expand-sm bg-light navbar-light">
+      <Link to="/" className="navbar-brand">
+        <img src={logo} alt="logo" />
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        onClick={() => setToggleNav(!toggleNav)}
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div
+        className={
+          toggleNav
+            ? "collapse navbar-collapse show"
+            : "collapse navbar-collapse"
+        }
+      >
+        <ul className="navbar-nav mx-auto">
+          <li className="nav-item">
+            <Link to="/" className="nav-link text-capitalize">
+              Test
+            </Link>
+          </li>
+          <li className="nav-item ml-sm-5">
+            <FaCartArrowDown className="cart-icon" />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,16 @@
 import PropTypes from "prop-types"
 import React from "react"
 import "./bootstrap.min.css"
+import Navbar from "./Globals/Navbar"
 import "./layout.css"
 
-const Layout = ({ children }) => <>{children}</>
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+
+    {children}
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
