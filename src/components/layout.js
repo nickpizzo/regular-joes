abe8a95fa1ca19eaf6a6a9ffc13e5cb068fd88a5
Nick Pizzo <nick.pizzo@gmail.com>
2019-03-06 14:18:45 -0800

    first commit

---
+++ src/components/layout.js
@@ -0,0 +1,19 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's StaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/static-query/
+ */
+
+import PropTypes from "prop-types"
+import React from "react"
+import "./bootstrap.min.css"
+import "./layout.css"
+
+const Layout = ({ children }) => <>{children}</>
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
