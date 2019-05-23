11195566229a98e305100222ff67c4c61e51b3a2
Nick Pizzo <nick.pizzo@gmail.com>
2019-05-23 13:14:41 -0700

    add and render disqus

--- src/templates/SinglePost.js
+++ src/templates/SinglePost.js
@@ -7,6 +7,8 @@ import { Card, CardBody, Badge, CardSubtitle, Row, Col } from "reactstrap"
 import Img from "gatsby-image"
 import slugify from "../util/utilityFunctions"
 import authors from "../util/authors"
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa"
+import { DiscussionEmbed } from "disqus-react"
 
 export const postQuery = graphql`
   query blogPostBySlug($slug: String!, $imageUrl: String!) {
@@ -37,9 +39,16 @@ export const postQuery = graphql`
   }
 `
 
-export default function SinglePost({ data }) {
+export default function SinglePost({ data, pageContext }) {
   const post = data.markdownRemark.frontmatter
   const author = authors.find(x => x.name === post.author)
+  const baseUrl = "https://gatsbytutorial.co.uk/"
+  const disqusShortname = "average-joes"
+  const disqusConfig = {
+    identifier: data.markdownRemark.id,
+    title: post.title,
+    url: baseUrl + pageContext.slug,
+  }
   return (
     <Layout>
       <SEO title={post.title} />
@@ -74,6 +83,57 @@ export default function SinglePost({ data }) {
                   </ul>
                 </CardBody>
               </Card>
+              <h3 className="text-center">Share This Post</h3>
+              <div className="text-center social-share-links">
+                <ul>
+                  <li>
+                    <a
+                      href={
+                        "https://www.facebook.com/sharer/sharer.php?u=" +
+                        baseUrl +
+                        pageContext.slug
+                      }
+                      className="facebook"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaFacebook />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={
+                        "https://twitter.com/share?url=" +
+                        baseUrl +
+                        pageContext.slug +
+                        "&text=" +
+                        post.title +
+                        "&via" +
+                        "twitterHandle"
+                      }
+                      className="twitter"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaTwitter />
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href={"https://www.linkedIn.com/shareArticle=" + baseUrl}
+                      className="linkedIn"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <FaLinkedin />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <DiscussionEmbed
+                shortname={disqusShortname}
+                congig={disqusConfig}
+              />
             </Col>
             <Col md="4">
               <Sidebar
